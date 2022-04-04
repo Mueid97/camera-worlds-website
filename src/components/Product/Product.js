378380ca170c595a1ca4fa915b876b1d37b43895
Mueid <mrmuyed579@gmail.com>
2022-04-04 17:32:20 +0600

    All done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
-    const { imgs, name, reating } = props.product;
+    const { review, imgs, name, reating } = props.product;
     return (
         <div className='review-item'>
             <div>
                 <img src={imgs} alt="" />
                 <h4>Name: {name}</h4>
+                <p>Review: <small>{review}</small></p>
                 <p>reating: {reating}</p>
             </div>
 
