1d85b28fc276b9223b1905f303a9a060ae334f0f
Mueid <mrmuyed579@gmail.com>
2022-06-11 23:27:53 +0600

    dashboard animatoin added

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -4,7 +4,7 @@ import CustomCharts from '../Chart/CustomCharts';
 import './DashBoard.css'
 const DashBoard = () => {
     return (
-        <div className='grid sm:grid-cols-1 lg:grid-cols-2 gap-20 mt-20 lg:px-24'>
+        <div data-aos="zoom-in-up" className='grid sm:grid-cols-1 lg:grid-cols-2 gap-20 mt-20 lg:px-24'>
             <AreaCharts></AreaCharts>
             <CustomCharts></CustomCharts>
         </div>
